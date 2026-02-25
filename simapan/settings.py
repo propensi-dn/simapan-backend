fe39f81ddc12d5fa5457d811eb479a4ad3910ca2
naphiale <nafia.levana19@gmail.com>
2026-02-25 17:57:30 +0700

    feat: add email configuration for SMTP backend with environment variables

--- simapan/settings.py
+++ simapan/settings.py
@@ -137,6 +137,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',

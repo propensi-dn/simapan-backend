67aa1af20af6aa73781482b65200906e95167055
naphiale <nafia.levana19@gmail.com>
2026-02-25 17:24:14 +0700

    feat: add savings and config apps to installed apps and update URL routing

--- simapan/settings.py
+++ simapan/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     # Local apps
     'users',
     'members',
+    'savings',
+    'config',
 ]
 
 MIDDLEWARE = [
--- simapan/urls.py
+++ simapan/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
-    path('api/members/', include('members.urls'))
+    path('api/members/', include('members.urls')),
+    path('api/savings/', include('savings.urls')),
+    path('api/config/', include('config.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

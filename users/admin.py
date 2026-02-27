cba346e7689141b7e6d03e7d973eba73c0959f07
naphiale <nafia.levana19@gmail.com>
2026-02-27 10:37:44 +0700

    feat: implement UserAdmin for managing User model in Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display  = ('email', 'role', 'is_active', 'is_staff', 'date_joined')
+    list_filter   = ('role', 'is_active', 'is_staff')
+    search_fields = ('email',)
+    ordering      = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Role & Status', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Permissions', {'fields': ('groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role', 'is_active', 'is_staff'),
+        }),
+    )

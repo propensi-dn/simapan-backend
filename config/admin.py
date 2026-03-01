0c26269d82dfea5a6b86572e324a28559906d3ad
nelsha <shakira.putrindari@gmail.com>
2026-03-02 01:04:39 +0700

    add: guest content config api

--- config/admin.py
+++ config/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import CooperativeBank
+from .models import CooperativeBank, LandingPageConfig, AboutConfig, FAQ, Service
 
 admin.site.register(CooperativeBank)
+admin.site.register(FAQ)
+admin.site.register(Service)
+
+@admin.register(LandingPageConfig)
+class LandingPageConfigAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False if LandingPageConfig.objects.exists() else True
+
+@admin.register(AboutConfig)
+class AboutConfigAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False if AboutConfig.objects.exists() else True
--- config/urls.py
+++ config/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
-urlpatterns = []
+from .views import HeroContentView, AboutContentView, FAQListView, ServiceListView
+
+urlpatterns = [
+    path('hero/', HeroContentView.as_view(), name='hero-content'),
+    path('services/', ServiceListView.as_view(), name='service-list'),
+    path('about/', AboutContentView.as_view(), name='about-content'),
+    path('faq/', FAQListView.as_view(), name='faq-list'),
+]

8fd04327b6088f56d4436fb38bfd1d412a43a33c
naphiale <nafia.levana19@gmail.com>
2026-03-10 03:02:09 +0700

    fix: use CompressedStaticFilesStorage for Django 6 compatibility

--- simapan/settings.py
+++ simapan/settings.py
@@ -104,11 +104,11 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
